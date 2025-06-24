98c76804453774e9ec0726e542079b7e690d1a09
Amir Alidoust <sahandalidoost@gmail.com>
2025-06-24 21:33:27 +0200

    Update WS.cpp

--- WS.cpp
+++ WS.cpp
@@ -2,16 +2,40 @@
 
 Servo myservo;  // create Servo object to control a servo
 
-int potpin = A0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+int potpin = A1;  // analog pin used to connect the potentiometer
+float val;    // variable to read the value from the analog pin
+
+int R2;
+int R1 = 120;
+int angle;
+
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+  myservo.attach(3);  // attaches the servo on pin 9 to the Servo object
+  Serial.begin(9600);
+
+  int best_angle = 0;
+  int opt_val = 1023;
+
+  for (angle=0; angle <= 180; angle += 5) {
+    myservo.write(angle);
+    delay(300);
+    int obs_val = analogRead(potpin);
+    if (obs_val < opt_val) {
+      opt_val = obs_val;
+      best_angle = angle;
+    }
+  }
+  myservo.write(best_angle);
+
 }
 
 void loop() {
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+
+  Serial.print("val= ");
+  Serial.println(val);
+
+  delay(100);                           // waits for the servo to get there
+
 }
