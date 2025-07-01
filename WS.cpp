7ab8d3689db3cad4a2c5267d6402b4697f491d83
Amir Alidoust <sahandalidoost@gmail.com>
2025-07-01 20:00:20 +0200

    Update WS.cpp

--- WS.cpp
+++ WS.cpp
@@ -1,40 +1,61 @@
 #include <Servo.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 Servo myservo;  // create Servo object to control a servo
 
-int potpin = A1;  // analog pin used to connect the potentiometer
-float val;    // variable to read the value from the analog pin
+int potpin = A1;    // analog pin used to connect the potentiometer
+float intensity;    // variable to read the value from the analog pin
 
 int R2;
 int R1 = 120;
-int angle;
+int best_angle;
+const INTERVALS = 2881;   // a day in seconds
+int curr_interval = 0;
+
+struct Records {
+  int angles[INTERVALS];
+  int intensities[INTERVALS];
+  int temperatures[INTERVALS];
+  int humidities[INTERVALS];
+}
+
+Records weather_data;
 
 
-void setup() {
-  myservo.attach(3);  // attaches the servo on pin 9 to the Servo object
-  Serial.begin(9600);
-
-  int best_angle = 0;
+int find_sun(int new_angle = 0){
   int opt_val = 1023;
-
-  for (angle=0; angle <= 180; angle += 5) {
+  for (int angle=0; angle <= 180; angle += 6) {
     myservo.write(angle);
     delay(300);
     int obs_val = analogRead(potpin);
     if (obs_val < opt_val) {
       opt_val = obs_val;
-      best_angle = angle;
+      new_angle = angle;
     }
   }
-  myservo.write(best_angle);
+  weather_data.angles[curr_interval]=new_angle;
+  weather_data.intensity[curr_interval]=opt_val;
+}
 
+
+void setup() {
+  myservo.attach(3);    // attaches the servo on pin 3 to the Servo object
+  Serial.begin(9600);
+  find_sun();
+  dht.begin();
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
 
-  Serial.print("val= ");
-  Serial.println(val);
+  for (int i = 0; i <= 86400; i+=30){
+    find_sun(best_angle);
+    delay(5000);
+  }
+
+  //Serial.print("intensity= ");
+  //Serial.println(intensity);
 
   delay(100);                           // waits for the servo to get there
 
